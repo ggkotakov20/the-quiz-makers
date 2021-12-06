40b561910f7aaad261b6bd713664c062c43fd221
SNChapkanov20 <85339002+SNChapkanov20@users.noreply.github.com>
2021-12-06 16:51:46 +0200

    random questions and bug fixes

--- app.cpp
+++ app.cpp
@@ -3,145 +3,177 @@
 
 using namespace std;
 
-void aboutUs()//Option 2
+//Option 2
+void aboutUs()
 {
     cout << setw(55) << "About us" << endl;
     cout << "Georgi Kotakov -> ggkotakov20@codingburgas.bg -> Backend Developer and SCRUM Trainer" << endl;
-    cout << "Stefan Chapkanov -> snchapkanov20@codingburgas.bg -> QA" << endl;
+    cout << "Stefan Chapkanov -> snchapkanov20@codingburgas.bg -> Quality Engineer" << endl;
     cout << "Kaloyan Dimov -> khdimov20@codingburgas.bg -> Backend Developer" << endl << endl;
 }
-void startGame()//Option 1
+//Option 1
+void startGame()
 {
     system("cls");//Clear menu
     char firstAnswer, secondAnswer, thirdAnswer, fourAnswer, fifthAnswer;
     double score = 0;
     int question = 0;
+    int questionNumber, pastQuestionsOrder[5];
 
-    //Question 1
-    cout << "The symbol of prefixes used for kilo is :" << endl;
-    cout << "a)M" << endl;
-    cout << "b)C" << endl;
-    cout << "c)K" << endl;
-    cout << "d)d" << endl;
+    for (int i = 0; i < 5; i++)
 
-    cin >> firstAnswer;
-
-    while (!(firstAnswer == 'a' || firstAnswer == 'b' || firstAnswer == 'c' || firstAnswer == 'd'))
     {
-        cout << "Incorrect input. Please try again!" << endl;
-        cin >> firstAnswer;
+        //Random questions
+        randNumber:
+        srand(time(0));
+        questionNumber = rand() % 5;
+
+        pastQuestionsOrder[i] = questionNumber;
+
+        for (int q = 0; q < i; q++)
+        {
+            if (questionNumber == pastQuestionsOrder[q])
+            {
+                goto randNumber;
+            }
+        }
+
+        //Question 1
+        if (questionNumber == 0)
+        {
+            cout << "The symbol of prefixes used for kilo is :" << endl;
+            cout << "a)M" << endl;
+            cout << "b)C" << endl;
+            cout << "c)K" << endl;
+            cout << "d)d" << endl;
+
+            cin >> firstAnswer;
+
+            while (!(firstAnswer == 'a' || firstAnswer == 'b' || firstAnswer == 'c' || firstAnswer == 'd'))
+            {
+                cout << "Incorrect input. Please try again!" << endl;
+                cin >> firstAnswer;
+            }
+
+            switch (firstAnswer)
+            {
+            case 'c':
+                score += 1;
+                break;
+            default:
+                break;
+            }
+            question += 1;
+        }
+
+        //Question 2
+        if (questionNumber == 1) {
+            cout << endl << "Which one of the following unit is not a derived unit?" << endl;
+            cout << "a)pascal" << endl;
+            cout << "b)Kilogramme" << endl;
+            cout << "c)Newton" << endl;
+            cout << "d)Watt" << endl;
+
+            cin >> secondAnswer;
+
+            while (!(secondAnswer == 'a' || secondAnswer == 'b' || secondAnswer == 'c' || secondAnswer == 'd'))
+            {
+                cout << "Incorrect input. Please try again!" << endl;
+                cin >> secondAnswer;
+            }
+
+            switch (secondAnswer)
+            {
+            case 'b':
+                score += 1;
+                break;
+            default:
+                break;
+            }
+            question += 1;
+        }
+
+        //Question 3
+        if (questionNumber == 2) {
+            cout << endl << "The instrument used to measure the depth is :" << endl;
+            cout << "a)Meter rule" << endl;
+            cout << "b)Vernier Callipers" << endl;
+            cout << "c)Screw guage" << endl;
+            cout << "d)Sold Cylinder" << endl;
+
+            cin >> thirdAnswer;
+
+            while (!(thirdAnswer == 'a' || thirdAnswer == 'b' || thirdAnswer == 'c' || thirdAnswer == 'd'))
+            {
+                cout << "Incorrect input. Please try again!" << endl;
+                cin >> thirdAnswer;
+            }
+
+            switch (thirdAnswer)
+            {
+            case 'b':
+                score += 1;
+                break;
+            default:
+                break;
+            }
+            question += 1;
+        }
+
+        //Question 4
+        if (questionNumber == 3) {
+            cout << endl << "The SI unit of mass it" << endl;
+            cout << "a)Gram" << endl;
+            cout << "b)Milligram" << endl;
+            cout << "c)Kilogram" << endl;
+            cout << "d)Centigram" << endl;
+
+            cin >> fourAnswer;
+
+            while (!(fourAnswer == 'a' || fourAnswer == 'b' || fourAnswer == 'c' || fourAnswer == 'd'))
+            {
+                cout << "Incorrect input. Please try again!" << endl;
+                cin >> fourAnswer;
+            }
+
+            switch (fourAnswer)
+            {
+            case 'c':
+                score += 1;
+                break;
+            default:
+                break;
+            }
+            question += 1;
+        }
+
+        //Question 5
+        if (questionNumber == 4) {
+            cout << endl << "There are ......... base quantities." << endl;
+            cout << "a)4" << endl;
+            cout << "b)6" << endl;
+            cout << "c)8" << endl;
+            cout << "d)7" << endl;
+
+            cin >> fifthAnswer;
+
+            while (!(fifthAnswer == 'a' || fifthAnswer == 'b' || fifthAnswer == 'c' || fifthAnswer == 'd'))
+            {
+                cout << "Incorrect input. Please try again!" << endl;
+                cin >> fifthAnswer;
+            }
+
+            switch (fifthAnswer)
+            {
+            case 'd':
+                score += 1;
+                break;
+            default:
+                break;
+            }
+            question += 1;
+        }
     }
-
-    switch (firstAnswer)
-    {
-    case 'c':
-        score += 1;
-        break;
-    default:
-        break;
-    }
-    question += 1;
-
-    //Question 2
-    cout << endl << "Which one of the following unit is not a derived unit?" << endl;
-    cout << "a)pascal" << endl;
-    cout << "b)Kilogramme" << endl;
-    cout << "c)Newton" << endl;
-    cout << "d)Watt" << endl;
-
-    cin >> secondAnswer;
-
-    while (!(secondAnswer == 'a' || secondAnswer == 'b' || secondAnswer == 'c' || secondAnswer == 'd'))
-    {
-        cout << "Incorrect input. Please try again!" << endl;
-        cin >> secondAnswer;
-    }
-
-    switch (secondAnswer)
-    {
-    case 'b':
-        score += 1;
-        break;
-    default:
-        break;
-    }
-    question += 1;
-
-    //Question 3
-    cout << endl << "The instrument used to measure the depth is :" << endl;
-    cout << "a)Meter rule" << endl;
-    cout << "b)Vernier Callipers" << endl;
-    cout << "c)Screw guage" << endl;
-    cout << "d)Sold Cylinder" << endl;
-
-    cin >> thirdAnswer;
-
-    while (!(thirdAnswer == 'a' || thirdAnswer == 'b' || thirdAnswer == 'c' || thirdAnswer == 'd'))
-    {
-        cout << "Incorrect input. Please try again!" << endl;
-        cin >> thirdAnswer;
-    }
-
-    switch (thirdAnswer)
-    {
-    case 'b':
-        score += 1;
-        break;
-    default:
-        break;
-    }
-    question += 1;
-
-    //Question 4
-    cout << endl << "The SI unit of mass it" << endl;
-    cout << "a)Gram" << endl;
-    cout << "b)Milligram" << endl;
-    cout << "c)Kilogram" << endl;
-    cout << "d)Centigram" << endl;
-
-    cin >> fourAnswer;
-
-    while (!(fourAnswer == 'a' || fourAnswer == 'b' || fourAnswer == 'c' || fourAnswer == 'd'))
-    {
-        cout << "Incorrect input. Please try again!" << endl;
-        cin >> fourAnswer;
-    }
-
-    switch (fourAnswer)
-    {
-    case 'c':
-        score += 1;
-        break;
-    default:
-        break;
-    }
-    question += 1;
-
-    //Question 5
-    cout << endl << "There are ......... base quantities." << endl;
-    cout << "a)4" << endl;
-    cout << "b)6" << endl;
-    cout << "c)8" << endl;
-    cout << "d)7" << endl;
-
-    cin >> fifthAnswer;
-
-    while (!(fifthAnswer == 'a' || fifthAnswer == 'b' || fifthAnswer == 'c' || fifthAnswer == 'd'))
-    {
-        cout << "Incorrect input. Please try again!" << endl;
-        cin >> fifthAnswer;
-    }
-
-    switch (fifthAnswer)
-    {
-    case 'd':
-        score += 1;
-        break;
-    default:
-        break;
-    }
-    question += 1;
-
 
     cout << endl << "Your score is " << (score / question) * 100 << "%" << endl;
     cout << "   _____              __  __   ______      ____   __      __  ______   _____   " << endl;
@@ -176,14 +208,14 @@ void mainList()
 
         cout << "Are you sure you want to exit the program ? " << endl << endl;
         cout << "Option 1 -> Yes" << endl;
-        cout << "Optiоn 2 -> No" << endl;
+        cout << "Option 2 -> No" << endl;
 
         cin >> quitChoice;
 
         switch (quitChoice)
         {
-        case '1'://Exit game
-            exit(0);
+        case '1':
+            exit(0);//Exit game
             break;
         case '2':
             break;
@@ -198,9 +230,9 @@ void mainList()
 
 int main()
 {
-    bool showMen = true;
+    bool showMenu = true;
 
-    while (showMen == 1)
+    while (showMenu == 1)
     {
         mainList();
     }
